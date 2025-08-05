89c57df1d4f342e34e0d3c26b4f951e2b2033f56
kk1212-ux <karthik.k119977@gmail.com>
2025-08-06 00:08:33 +0530

    modified rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -61,36 +61,3 @@ def play_game():
 
 if __name__ == "__main__":
     play_game()
-
-
-
-
-
-
-
-# import random
-# OPTIONS = ("rock","paper","scissors")
-# running = True
-
-# while running:
-#     player = None
-#     computer = random.choice(OPTIONS)
-
-#     while player not in OPTIONS:
-#         player = input("Enter rock, paper, scissors: ").lower()
-#     print(f"Player: {player}")
-#     print(f"Computer: {computer}")
-#     if player == computer:
-#         print("It's a tie!")
-#     elif player == "rock" and computer == "scissors":
-#         print("You win! rock beats scissors.")
-#     elif player == "paper" and computer == "rock":
-#         print("You win! paper beats rock.")
-#     elif player == "scissors" and computer == "paper":
-#         print("You win! scissors beats paper.")
-#     else:
-#         print(f"You lose!")
-#     play_again = input("Do you want to play again? (y/n): ").lower()
-#     if play_again != "y":
-#         running = False
-# print("Thanks for playing")
